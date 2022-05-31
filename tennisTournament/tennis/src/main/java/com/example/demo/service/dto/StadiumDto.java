a169ec5d6da79b16b0fc4498d90bafd518335dc7
Quang Phong Ly Minh <plmquang@axonactive.com>
2022-05-31 09:45:19 +0700

    update stadium DTO

--- tennisTournament/tennis/src/main/java/com/example/demo/service/dto/StadiumDto.java
+++ tennisTournament/tennis/src/main/java/com/example/demo/service/dto/StadiumDto.java
@@ -1,2 +1,26 @@
-package com.example.demo.service.dto;public class StadiumDto {
+package com.example.demo.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StadiumDto {
+    private Integer stadiumId;
+
+    private String stadiumName;
+
+    private String stadiumAddress;
+
+    private Integer seatCapacity;
+
+    private String stadiumLocation;
+
+    private LocalTime openHour;
+
+    private LocalTime closeHour;
 }
